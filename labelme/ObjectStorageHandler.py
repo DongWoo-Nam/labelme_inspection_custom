4cf555211bdaaccbaffcd74bfb32c21157c03fde
hskim <hskim@12cm.co.kr>
2021-09-07 13:32:23 +0900

    다운로드 업로드 수정

--- labelme/ObjectStorageHandler.py
+++ labelme/ObjectStorageHandler.py
@@ -132,12 +132,19 @@ def get_all_keys(**args):
 
 # 오브젝트 다운로드
 def download_object_by_client(bucket_name, object_name, save_path):
-    s3_down.download_file(bucket_name, object_name, save_path)
+    file_path = os.path.dirname(object_name)
+    if os.path.isfile(save_path + object_name):
+        return
+    if not os.path.exists(save_path + file_path):
+        os.makedirs(save_path + file_path)
+    s3_down.download_file(bucket_name, object_name, save_path + object_name)
 
 
 # 오브젝트 다운로드
 def download_object(object_name, save_path, s3bucket):
     file_path = os.path.dirname(object_name)
+    print("Downloading object : %s" % object_name)
+
     if os.path.isfile(save_path + object_name):
         return
     if not os.path.exists(save_path + file_path):
@@ -155,7 +162,7 @@ def download_directory_by_client(bucket_name, directory_name, save_path, login_i
     items = get_object_list_directory_all(bucket_name=bucket_name, prefix=login_id, extension=['png', 'jpeg', 'jpg'])
 
     total_items = len(items)
-    progress = QtWidgets.QProgressDialog("Download files...", QtCore.QString(), 0, total_items)
+    progress = QtWidgets.QProgressDialog("Download files...", "", 0, total_items)
     progress.setWindowTitle("Downloading files...")
     progress.setCancelButton(None)
     progress.setAutoClose(True)
@@ -164,8 +171,8 @@ def download_directory_by_client(bucket_name, directory_name, save_path, login_i
 
     for item in items:
         print('item: %d key: %s' % (progress.value(), item))
-        item_save_path = save_path + '/' + str(item.rsplit('/')[-1])
-        download_object_by_client(bucket_name=bucket_name, object_name=item, save_path=item_save_path)
+        # item_save_path = save_path + '/' + str(item.rsplit('/')[-1])
+        download_object_by_client(bucket_name=bucket_name, object_name=item, save_path=save_path)
 
         print('Downloading files...  %s/%s' % (str(progress.value()), str(total_items)))
         progress.setLabelText = 'Downloading files... ' + str(progress.value()) + '/' + str(total_items)
@@ -197,7 +204,7 @@ def upload_object(bucket_name, local_file_path, directory):
     s3bucket = s3.Bucket(bucket_name)
     # s3_up.put_object(Bucket=bucket_name, Key=directory)
     # 업로드할 오브젝트명 설정
-    object_name = local_file_path.split("labelme\\")[1].replace(os.path.sep, "/")  # 흰다리 새우에서만 사용 가능
+    object_name = local_file_path.split("labelme" + os.path.sep)[1].replace(os.path.sep, "/")  # 흰다리 새우에서만 사용 가능
     # 파일 업로드
     print("local_file_path={}".format(local_file_path))
     print("bucket_name={}".format(bucket_name))
@@ -211,7 +218,8 @@ def upload_object_by_client(bucket_name, local_file_path, directory):
     # 디렉토리 생성(디렉토리가 존재하지 않으면 생성)
     s3_up.put_object(Bucket=bucket_name, Key=directory)
     # 업로드할 오브젝트명 설정
-    object_name = directory + "/" + local_file_path.rsplit(os.path.sep)[-1]
+    object_name = local_file_path.split("labelme" + os.path.sep)[1].replace(os.path.sep, "/")  # 흰다리
+    # object_name = directory + "/" + local_file_path.rsplit(os.path.sep)[-1]
     # 파일 업로드
     print("local_file_path={}".format(local_file_path))
     print("bucket_name={}".format(bucket_name))
